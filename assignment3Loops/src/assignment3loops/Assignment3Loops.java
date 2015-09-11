3f76651e1f6e5723a2434c638d3ffd579d3e91c7
Samantha Shurie <s_shurie@yahoo.com>
2015-09-10 22:50:22 -0400

    While Loop
    added while loop to verify that the word is at least 5 characters long

--- assignment3Loops/src/assignment3loops/Assignment3Loops.java
+++ assignment3Loops/src/assignment3loops/Assignment3Loops.java
@@ -3,9 +3,10 @@ package assignment3loops;
 
 import java.util.Scanner; //needed for scanner 
 
-/**
- *
- * @author Samantha Shurie
+/**Programmer: Samantha Shurie
+ * CPT 163-27: Java I
+ * Date: 9/10/15
+ * Email: shuries@my.wccc.edu
  */
 public class Assignment3Loops {
 
@@ -20,6 +21,12 @@ public class Assignment3Loops {
         System.out.print("Enter a word to reverse: ");
         word = keyboard.nextLine();
         
+        //validate entry
+        while(word.length() < 5) {
+            System.out.print("*Please enter a word that is 5 characters or longer: ");
+            word = keyboard.nextLine();
+        }
+            
         //loop word
         /*start at (length - 1(last character)), and stop at 0,
         /*decrementing by 1. Store character in this position in a variable
@@ -29,8 +36,7 @@ public class Assignment3Loops {
             reverseWord = reverseWord + letter;
         }
         //output word :)
-        System.out.print("Your word backward---> "
-                        + reverseWord);
+        System.out.print("Your word backwards---> " + reverseWord);
         
     }
     
